a417bfacc32c0d429716a4944a47ff0689580a9f
Emilie Dunphy <emilie.dunphy@gmail.com>
2017-10-25 14:27:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,8 +22,8 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 function accessElementInArray(array, index) {
-console.log(array, index);
-return array;
+console.log(array(index));
+return array
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
