807e8fb301e97502e2f15bc8c82aaff0323494da
Emilie Dunphy <emilie.dunphy@gmail.com>
2017-10-25 14:56:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 function accessElementInArray(array, index) {
-  console.log(array.index);
+  array = console.log(array[index]);
   return index;
 }
 
